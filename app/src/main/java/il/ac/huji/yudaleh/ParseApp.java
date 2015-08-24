f63b15f77d89ada0689830baa49a77eaaec4a2a2
eilon <eilon.karty@mail.huji.ac.il>
2015-08-24 21:08:56 +0300

    login

--- app/src/main/java/il/ac/huji/yudaleh/ParseApp.java
+++ app/src/main/java/il/ac/huji/yudaleh/ParseApp.java
@@ -1,12 +1,15 @@
 package il.ac.huji.yudaleh;
 
 import com.parse.Parse;
+import com.parse.ParseACL;
 import com.parse.ParseInstallation;
+import com.parse.ParseUser;
 import com.parse.PushService;
+import android.app.Application;
 
-public class Application extends android.app.Application {
+public class ParseApp extends Application {
 
-  public Application() {
+  public ParseApp() {
   }
 
   @Override
@@ -16,8 +19,16 @@ public class Application extends android.app.Application {
 	// Initialize the Parse SDK.
 	Parse.initialize(this, "1wOBsSzT94l7KHNBFfofmIg0VvpAVVO2o9K7GXoF", "M6unV2mvfdN7e24AnoJ9GTE67YjWTf0jZI7Ky3LZ");
 
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defauAcl = new ParseACL();
+
+    defauAcl.setPublicReadAccess(true);
+    ParseACL.setDefaultACL(defauAcl, true);
+
 	// Specify an Activity to handle all pushes by default.
 	PushService.setDefaultPushCallback(this, MainActivity.class);
-//    ParseInstallation.getCurrentInstallation().saveInBackground();
+
+//    ParseInstallation.getCurrentInstallation().saveInBackground();todo remove
   }
 }
